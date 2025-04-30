7594f0689e6a1c1515c24bf6b4ca14eeb59de65a
Ahmar Ali <ahmarali2004@gmail.com>
2025-04-30 16:01:06 +0500

    Create linkedList.h

---
+++ DataStructuresUsed/linkedList.h
@@ -0,0 +1,26 @@
+
+
+//header file for templatized linked list:
+
+
+//array-based version:
+
+template <class T>
+class arrayList
+{
+
+};
+
+//pointer based version:
+
+template <class T>
+class nodeList
+{
+
+};
+
+template <class T>
+class linkedList
+{
+
+};
