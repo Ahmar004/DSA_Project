92a3a9a717a133f927753d8be44b58981eae0d7a
Ahmar Ali <ahmarali2004@gmail.com>
2025-04-30 15:54:08 +0500

    Create minHeap.h

---
+++ DataStructuresUsed/minHeap.h
@@ -0,0 +1,38 @@
+//
+//header file for min-heap:
+//
+
+
+//array version:
+
+template <class T>
+class minHeapArray
+{
+
+public:
+
+
+};
+
+
+
+//linkedlist version:
+
+template <class T>
+class nodeHeap
+{
+
+public:
+
+
+};
+
+template <class T>
+class minHeapList
+{
+
+public:
+
+
+};
+
