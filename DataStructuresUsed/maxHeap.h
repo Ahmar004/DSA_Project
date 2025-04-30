6e1e563cc0aa50a672a228dfdc5b9a9bd9744aae
Ahmar Ali <ahmarali2004@gmail.com>
2025-04-30 15:56:42 +0500

    Update and rename maxHeap

---
+++ DataStructuresUsed/maxHeap.h
@@ -0,0 +1,37 @@
+//
+//header file for max-heap:
+//
+
+
+//array version:
+
+template <class T>
+class maxHeapArray
+{
+
+public:
+
+
+};
+
+
+
+//linkedlist version:
+
+template <class T>
+class nodeMaxHeap
+{
+
+public:
+
+
+};
+
+template <class T>
+class maxHeapList
+{
+
+public:
+
+
+};
